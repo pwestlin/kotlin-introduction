f21d5adffec99c614977cd6c130b2ae37beacca6
Peter Vestlin <peter.vestlin@lm.se>
2018-10-17 15:33:04 +0200

    First

---
+++ src/test/kotlin/se/lantmateriet/taco/kotlin/BasicsKtTest.kt
@@ -0,0 +1,473 @@
+package se.lantmateriet.taco.kotlin
+
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.io.IOException
+import java.time.LocalDate
+import java.time.chrono.ChronoLocalDate
+import kotlin.system.measureTimeMillis
+
+class BasicsKtTest {
+
+    @Test
+    // Funktion med specad returtyp och "body"
+    fun functionWithReturnTypeAndBody() {
+        fun double(int: Int): Int {
+            return int * 2
+        }
+
+        assertThat(double(3)).isEqualTo(6)
+    }
+
+    @Test
+    fun functionWithExpressionBodyAndAutomaticReturnType() {
+        fun square(int: Int) = int * int
+
+        assertThat(square(3)).isEqualTo(9)
+    }
+
+    @Test
+    fun noReturnTypeForFunction() {
+        fun voidFunctionOrWhat() {
+            1 + 1
+        }
+
+        // Returtypen void saknas, utan kallas Unit och kan utelämnas vid deklaration.
+        assertThat(voidFunctionOrWhat()).isInstanceOf(Unit::class.java)
+    }
+
+    @Test
+    fun variables() {
+        // Definiera varibel
+        val i: Int = 5
+
+        // En till Int med automatisk typ Int
+        val j = 5
+
+        val k = 7
+        //k = 8 -> kompileringsfel eftersom k är en val som inte kan ändras
+
+        var l = 8
+        l = 9   // Detta går bra eftersom l är en var
+    }
+
+    @Test
+    fun stringTemplates() {
+        val name = "Peter"
+        val age = 46
+
+        assertThat("$name är $age år gam...ung").isEqualTo("Peter är 46 år gam...ung")
+    }
+
+
+    @Test
+    fun conditionalExpressions() {
+        // Conditional statements returnerar värden!
+        val strFromCond = if (2 > 1) 2 else 1
+
+        assertThat(strFromCond).isEqualTo(2)
+    }
+
+    @Test
+    fun nulls() {
+        var string: String = "a"
+        //string = null     // Kompileringsfel för var string får inte vara null
+
+        // Notera ? efter String
+        var nullableString: String? = "b"
+        nullableString = null
+
+        assertThatThrownBy { nullableString!!.length }
+            .isInstanceOf(KotlinNullPointerException::class.java)
+            .hasMessage(null)
+    }
+
+    @Test
+    // Jämför med Javas switch
+    fun whenExpressions() {
+        val obj: Any = 0.23
+
+        val string =
+            when (obj) {
+                1 -> "One"
+                "Hello" -> "Greeting"
+                is Long -> "Long"
+                !is String -> "Not a string"
+                else -> "Unknown"
+            }
+
+        assertThat(string).isEqualTo("Not a string")
+    }
+
+    @Test
+    fun ranges() {
+        val x = 5
+        val y = 7
+        if (x in 1..y) {
+            println("in range")
+        }
+
+        // progressions
+        for (x in 1..10 step 2) {
+            print("$x ")
+        }
+        println()
+        for (x in 9 downTo 0 step 3) {
+            print("$x ")
+        }
+    }
+
+    @Test
+    fun collections() {
+        val fruits = listOf("banana", "avocado", "apple", "kiwifruit", "orange")
+
+        when {
+            "orange" in fruits -> println("juicy")
+            "apple" in fruits -> println("apple is fine too")
+            else -> println("whatever")
+        }
+
+        // Inget anrop till stream()
+        fruits
+            .filter { it.startsWith("a") }
+            .sortedBy { it }
+            .map { it.toUpperCase() }
+            .forEach { println(it) }
+    }
+
+    @Test
+    fun defaultValuesForFunctionParams() {
+        // Japp, man kan ha funktioner inuti en funktion
+        fun multiply(a: Int, b: Int = 2) = a * b
+
+        assertThat(multiply(5, 5)).isEqualTo(25)
+        assertThat(multiply(5)).isEqualTo(10)
+
+        // Jfr med Javas overloaded methods som anropar varandra
+    }
+
+    @Test
+    fun nullableCheck() {
+        val files = File("/asfgsdhd/cvhndf/jmfghjern").listFiles()    // listFiles() returnerar null om inte pathen kan hittas
+        assertThat(files).isNull()
+
+        assertThatThrownBy { files.size }
+            .isInstanceOf(NullPointerException::class.java)
+            .hasMessage(null)
+
+        assertThat(files?.size).isNull()
+        println(files?.size)    // Java: if(files != null) {System.out.println(files.size)}
+
+        println(files?.size ?: "empty")     // Java: *puh*
+
+        var value: String? = null
+        value?.let {
+            println("Inte null")
+        }
+        value = "foo"
+        value?.let {
+            println("Inte null")
+        }
+    }
+
+    @Test
+    fun tryCatch() {
+        // try-satser kan returnera ett värde
+        val value = try {
+            Integer.parseInt("7")
+        } catch (e: NumberFormatException) {
+            e.printStackTrace()
+        }
+
+        println("value = $value")
+    }
+
+    @Test
+    fun checkedExceptions() {
+        // Kotlin does not have checked exceptions - YAAAAAAAAAAAAAAAAY!
+        // ...men hur sjutton blir det när man anropar en Java-metod som kastar ett checked exception?
+
+        // readBytes() kastar ett IOException (checked exception) om en fil inte kan tas bort
+        //println(File("/d2362/sdhdhs/xbryud/sdfghsdg.txt").readBytes())
+
+        // ...men om jag vill ta hand om felet då?
+        try {
+            println(File("/d2362/sdhdhs/xbryud/sdfghsdg.txt").readBytes())
+        } catch (e: IOException) {
+            println("Do something")
+            e.printStackTrace()
+        }
+    }
+
+    @Test
+    fun callingMultipleMethodsOnAnObjectInstance() {
+        class Turtle {
+            fun penDown() = println("Down")
+            fun penUp() = println("Up")
+            fun turn(degrees: Double) = println("turning $degrees")
+            fun forward(pixels: Double) = println("forward $pixels")
+        }
+
+        val myTurtle = Turtle()
+        // with "väljer" en objektinstans jag vill jobba med i kontextet
+        with(myTurtle) {
+            //draw a 100 pix square
+            penDown()
+            for (i in 1..4) {
+                forward(100.0)
+                turn(90.0)
+            }
+            penUp()
+        }
+    }
+
+    @Test
+    fun classesAndObjects() {
+        // Japp, man kan skapa klasser inuti en funktion
+        class Car(val brand: String, var name: String)
+
+        val car = Car("Volvo", "Pärlan")
+        assertThat(car.brand).isEqualTo("Volvo")
+        assertThat(car.name).isEqualTo("Pärlan")
+
+        //car.brand = "Opel"    // Näru, detta går inte!
+        car.name = "Skrothögen"
+        assertThat(car.brand).isEqualTo("Volvo")
+        assertThat(car.name).isEqualTo("Skrothögen")
+
+        println("car = ${car}")
+    }
+
+    @Test
+    fun classesAndObjects_dataClasses() {
+        data class Person(val name: String, val age: Int)
+
+        val person = Person("Peter", 46)
+        println("person = ${person}")
+        println("${person.name} är ${person.age} år gam...ung")
+
+        assertThat(person.name).isEqualTo("Peter")
+        assertThat(person.age).isEqualTo(46)
+    }
+
+    object Resource {
+        const val name = "I am a singleton"
+        const val value = 42
+
+
+    }
+
+    @Test
+    fun singleton() {
+        assertThat(Resource.name).isEqualTo("I am a singleton")
+        assertThat(Resource.value).isEqualTo(42)
+    }
+
+    class Person private constructor(val name: String) {
+        companion object Factory {
+            fun create(firstname: String, surname: String): Person = Person("$firstname $surname")
+        }
+    }
+
+    @Test
+    fun companionObjects() {
+        val myClass = Person.create("Pelle", "Plutt")
+
+        assertThat(myClass.name).isEqualTo("Pelle Plutt")
+    }
+
+    @Test
+    fun extensionFunctions() {
+        // Här börjar det bli fränt på riktigt!
+
+        val string = "foo"
+        assertThat(string.reversed().toUpperCase()).isEqualTo("OOF")
+        // Vi vill att det ska finnas en funktion för att reversera strängen och göra den till versaler
+
+        fun String.reverseAndToUpperCase() = this.reversed().toUpperCase()
+        assertThat(string.reverseAndToUpperCase()).isEqualTo("OOF")
+
+        // Ni vet den där klassen StringUtils eller DateUtils som man ALLTID har i VARJE projekt?
+        // ...den behövs inte längre för man kan använda extension functions istället! :)
+    }
+
+    enum class Motorcycle(val brand: String) {
+        SUZUKI("Suzuki"),
+        HONDA("Honda")
+    }
+
+    @Test
+    fun enums() {
+        assertThat(Motorcycle.valueOf("HONDA")).isEqualTo(Motorcycle.HONDA)
+        assertThat(Motorcycle.SUZUKI.brand).isEqualTo("Suzuki")
+    }
+
+    @Test
+    fun functionDefaultAndNamedArgs() {
+        fun reformat(string: String, reverse: Boolean = false, upperCase: Boolean = false): String {
+            var formatted = string
+            if (reverse) {
+                formatted = formatted.reversed()
+            }
+            if (upperCase) {
+                formatted = formatted.toUpperCase()
+            }
+
+            return formatted
+        }
+
+        assertThat(reformat("Elsa i Paris")).isEqualTo("Elsa i Paris")
+        assertThat(reformat("Elsa i Paris", true)).isEqualTo("siraP i aslE")
+        assertThat(reformat("Elsa i Paris", true, true)).isEqualTo("SIRAP I ASLE")
+        assertThat(reformat("Elsa i Paris", upperCase = true)).isEqualTo("ELSA I PARIS")
+    }
+
+    @Test
+    fun lambda_compute() {
+        val add = { a: Int, b: Int -> a + b }
+        val subtract = { a: Int, b: Int -> a - b }
+        val multiply = { a: Int, b: Int -> a * b }
+
+        fun compute(a: Int, b: Int, function: (Int, Int) -> Int): Int {
+            return function(a, b)
+        }
+
+        assertThat(compute(4, 2, add)).isEqualTo(6)
+        assertThat(compute(4, 2, subtract)).isEqualTo(2)
+        assertThat(compute(4, 2, multiply)).isEqualTo(8)
+        assertThat(compute(4, 2, { a: Int, b: Int -> a / b })).isEqualTo(2)
+    }
+
+    @Test
+    fun collections2() {
+        val numbers = listOf(1, 2, 3)
+        assertThat(numbers).containsExactly(1, 2, 3)
+        //numbers.add(4)    // add finns inte som funktion på en immutable list
+
+        val numbers2 = mutableListOf(1, 2, 3)
+        assertThat(numbers2).containsExactly(1, 2, 3)
+        numbers2.add(4)
+        assertThat(numbers2).containsExactly(1, 2, 3, 4)
+    }
+
+    @Test
+    fun typeChecks() {
+        fun printLength(obj: Any) {
+            if (obj is String) {
+                println(obj.length)
+            }
+
+            if (obj !is String) { // same as !(obj is String)
+                println("Not a String")
+            } else {
+                println(obj.length)
+            }
+        }
+
+        printLength("foo")
+        printLength(123)
+    }
+
+    @Test
+    fun smartCasts() {
+        //val date: ChronoLocalDate? = LocalDate.now()
+        val date: ChronoLocalDate? = null
+        if (date != null) {
+            println(date.isLeapYear)
+        }
+
+        if (date != null && date.isLeapYear) {
+            println("It's a leap year!")
+        }
+
+        if (date == null || !date.isLeapYear) {
+            println("There's no Feb 29 this year...")
+        }
+
+        if (date is LocalDate) {
+            val month = date.monthValue
+            println(month)
+        }
+
+        /////////////////////////
+
+        fun foo(obj: Any) {
+            if (obj is String) {
+                println("obj.length = ${obj.length}")
+            }
+        }
+
+        foo("sgsx")
+        foo(72.3)
+    }
+
+    data class Car(val brand: String, val model: String, val year: String)
+
+    @Test
+    fun equality() {
+        val car = Car("Volvo", "PV", "1970")
+
+        val equalCar = Car("Volvo", "PV", "1970")
+        assertThat(equalCar == car).isTrue()
+
+        val notEqualCar = Car("Volvo", "Amazon", "1966")
+        assertThat(notEqualCar == car).isFalse()
+
+        assertThat(notEqualCar === car).isFalse()
+
+        val sameCar = car
+        assertThat(sameCar == car).isTrue()
+        assertThat(sameCar === car).isTrue()
+    }
+
+
+    data class User(val username: String, val email: String)
+
+    @Test
+    fun destructuringDeclarations() {
+        val user = User("bosseringholm", "bosse_man@oldfolkshome.se")
+
+        val (username, email) = user
+        assertThat(username).isEqualTo("bosseringholm")
+        assertThat(email).startsWith("bosse_man")
+    }
+
+    @Test
+    fun coroutines() {
+        val time = measureTimeMillis {
+            runBlocking {
+                repeat(100_000) {
+                    // launch a lot of coroutines
+                    launch {
+                        delay(1000L)
+                        print(".")
+                    }
+                }
+            }
+        }
+        println("\ntime = $time ms")
+    }
+
+
+    inline fun <T> doStuffSlowly(block: () -> T): T {
+        val start = System.currentTimeMillis()
+        Thread.sleep(100)
+        val retVal = block()
+        Thread.sleep(100)
+        println(System.currentTimeMillis() - start)
+        return retVal
+    }
+
+
+    @Test
+    fun inlineStuff() {
+        val multiply = { a: Int, b: Int -> a * b }
+        val retVal = doStuffSlowly { multiply(7, 7) }
+        println("retVal = ${retVal}")
+    }
+}
