78078c51cd3519021f0202f42837862dadbd9778
Peter Vestlin <peter.vestlin@lm.se>
2018-10-18 14:57:54 +0200

    Pytt

--- src/test/kotlin/se/lantmateriet/taco/kotlin/basics/BasicsKtTest.kt
+++ src/test/kotlin/se/lantmateriet/taco/kotlin/basics/BasicsKtTest.kt
@@ -281,6 +281,17 @@ class BasicsKtTest {
         assertThat(Resource.value).isEqualTo(42)
     }
 
+
+
+
+    /*
+    In Kotlin, unlike Java or C#, classes do not have static methods. In most cases, it's recommended to simply use package-level functions instead.
+    If you need to write a function that can be called without having a class instance but needs access to the internals of a class (for example, a factory method),
+    you can write it as a member of an object declaration inside that class.
+    Even more specifically, if you declare a companion object inside your class, you'll be able to
+    call its members with the same syntax as calling static methods in Java/C#, using only the class name as a qualifier.
+     */
+
     class Person private constructor(val name: String) {
         companion object Factory {
             fun create(firstname: String, surname: String): Person = Person("$firstname $surname")
