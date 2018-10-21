381b1731af58406c72d0be3e7f5cd215e01bc8f7
Peter Vestlin <peter.vestlin@lm.se>
2018-10-21 08:25:05 +0200

    Fler tester

--- src/test/kotlin/se/lantmateriet/taco/kotlin/learn_kotlin/lessbasic/DelegatedPropertiesTest.kt
+++ src/test/kotlin/se/lantmateriet/taco/kotlin/learn_kotlin/lessbasic/DelegatedPropertiesTest.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UNUSED_VARIABLE", "UNUSED_VALUE", "RedundantExplicitType", "ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE", "VARIABLE_WITH_REDUNDANT_INITIALIZER", "ALWAYS_NULL", "UNNECESSARY_SAFE_CALL", "EXPERIMENTAL_FEATURE_WARNING", "MemberVisibilityCanBePrivate", "SimplifyBooleanWithConstants", "ConstantConditionIf", "MoveLambdaOutsideParentheses", "UnnecessaryVariable", "unused", "UNUSED_PARAMETER", "RemoveRedundantBackticks", "NullChecksToSafeCall", "LiftReturnOrAssignment", "ReplaceGetOrSet", "NonAsciiCharacters", "PackageName", "ClassName")
+
 package se.lantmateriet.taco.kotlin.learn_kotlin.lessbasic
 
 import org.assertj.core.api.Assertions
