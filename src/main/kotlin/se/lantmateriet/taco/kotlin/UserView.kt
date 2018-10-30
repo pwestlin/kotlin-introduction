ea171f140eaa509cdb9f73cce4ae02dbba21e3ad
Peter Vestlin <peter.vestlin@lm.se>
2018-10-30 15:02:04 +0100

    Fler tester

--- src/main/kotlin/se/lantmateriet/taco/kotlin/UserView.kt
+++ src/main/kotlin/se/lantmateriet/taco/kotlin/UserView.kt
@@ -2,7 +2,7 @@ package se.lantmateriet.taco.kotlin
 
 import se.lantmateriet.taco.java.User
 
-// Skapa en extension function på User som konverterar till en UserView
+// Skapa en extension function på User (som är en Java-klass) som konverterar till en UserView
 fun User.toUserView() = UserView(username, password, "$firstname $lastname")
 
 data class UserView(
